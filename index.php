cc7a11ff54274a131ef017d3d0d4dcc54c8497e7
Mads Hansen <mads@mediastyle.dk>
2013-06-04 19:19:33 +0200

    Merge branch 'master' of https://github.com/Deloux/repo
    Conflicts:
    index.php

--- index.php
+++ index.php
@@ -1,1 +1,10 @@
 <?php echo 'Local commit'; ?>
+<<<<<<< HEAD
+<<<<<<< HEAD
+<?php echo 'conflict commit'; ?>
+=======
+<?php echo 'server commit'; ?>
+>>>>>>> c0b23f3fa5165440d526f133767c32ea45b945b7
+=======
+<?php echo 'server commit'; ?>
+>>>>>>> c0b23f3fa5165440d526f133767c32ea45b945b7
